780773fa2b544ce2c3b1fd799e772936f0363544
Sathya <Sathya@sathya.com>
2022-08-05 11:28:43 +0530

    Changes

--- go-api/tests/setup_test.go
+++ go-api/tests/setup_test.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
-	"github.com/victorsteven/fullstack/api/controllers"
-	"github.com/victorsteven/fullstack/api/models"
+	"github.com/sathyamurthyb4u/fullstack/api/controllers"
+	"github.com/sathyamurthyb4u/fullstack/api/models"
 )
 
 var server = controllers.Server{}
