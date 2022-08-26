f1b9cedb7f6a5ac8d99c9c185124da4862ad73ec
Sathya <Sathya@sathya.com>
2022-08-26 17:57:58 +0530

    Prefect 2.0

---
+++ prefect-python/2.0-WIP/flows/init_orion.py
@@ -0,0 +1,91 @@
+import requests
+import time
+
+
+OUTPUT_ENV_FILE = './prefect.env'
+
+ORION_SERVER_URL = 'http://localhost:4200'
+GET_BLOCK_TYPES_URL = f'{ORION_SERVER_URL}/api/block_schemas/filter'
+CREATE_BLOCK_URL = f'{ORION_SERVER_URL}/api/block_documents'
+CREATE_WORK_QUEUE_URL = f'{ORION_SERVER_URL}/api/work_queues'
+SET_DEFAULT_STORAGE_URL = f'{ORION_SERVER_URL}/api/block_documents/1499dc8f-c48b-4535-afda-24d7e0f5dd42/set_default_storage_block_document'
+
+session = requests.Session()
+
+
+def init_storage(storage_type: str = 'File Storage', name: str = 'minio_docker'):
+    storage_type: str = 'File Storage'
+    name: str = 'minio_docker'
+    # get block spec id for the storage type we want
+    resp = session.post(GET_BLOCK_TYPES_URL, json={})
+    resp.raise_for_status()
+    block_schema = [ x for x in resp.json() if x['block_type']['name'] == storage_type ]
+    if not block_schema:
+        raise ValueError('<File Storage> not found')
+    block_schema = block_schema[0]
+    block_type_id = block_schema['block_type_id']
+    block_schema_id = block_schema['id']
+
+    # create storage block
+    storage_options = dict(
+        use_ssl = False,
+        key = "one!one!",
+        secret = "one!one!",
+        client_kwargs = dict(endpoint_url = "http://minio:9000")
+    )
+    payload = dict(
+        name = name,
+        block_type_id = block_type_id,
+        block_schema_id = block_schema_id,
+        data = dict(base_path='s3://prefect-flows/', key_type="hash", options=storage_options)
+    )
+    resp = session.post(CREATE_BLOCK_URL, json=payload)
+    resp.raise_for_status()
+    block_id = resp.json()['id']
+    # set sotrage as default
+    resp = session.post(SET_DEFAULT_STORAGE_URL.replace('1499dc8f-c48b-4535-afda-24d7e0f5dd42', block_id), json={})
+    resp.raise_for_status()
+    return block_id
+
+
+def init_work_queue(name='docker_queue'):
+    payload = dict(
+        name = name,
+        filter = dict(
+            flow_runner_types = ['docker']
+        )
+    )
+    resp = session.post(CREATE_WORK_QUEUE_URL, json=payload)
+    resp.raise_for_status()
+    return resp.json()['id']
+
+
+def wait_for_server_ready(max_wait_seconds: int = 60):
+    up = False
+    deadline = time.time() + max_wait_seconds
+    while time.time() <= deadline:
+        print('waiting for prefect server to be ready...')
+        try:
+            resp = session.get(f'{ORION_SERVER_URL}/api/health')
+            resp.raise_for_status()
+            up = resp.json()
+            if up is True:
+                break
+        except Exception:
+            pass
+        time.sleep(2)
+    if not up:
+        raise RuntimeError(f'prefect server not ready after {max_wait_seconds} seconds')
+
+
+def main():
+    wait_for_server_ready()
+    storage_id = init_storage()
+    queue_id = init_work_queue()
+    with open(OUTPUT_ENV_FILE, 'w') as f:
+        f.write(f'STORAGE_ID={storage_id}\n')
+        f.write(f'QUEUE_ID={queue_id}')
+
+
+if __name__ == '__main__':
+    main()
